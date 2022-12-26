2776573dba090492ddc507de9cea74759a04b87e
Sihkdragon <ditolord@gmail.com>
2022-12-26 22:58:33 +0700

    move layout to its place

--- components/templates/BaseLayout.tsx
+++ components/templates/BaseLayout.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import React, { useState } from "react";
+import Sidebar from "../molecules/Sidebar";
+import Topbar from "../molecules/Topbar";
 
 const BaseLayout = ({
   children,
@@ -9,11 +11,23 @@ const BaseLayout = ({
   PageTitle: string;
 }) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNav = () => setIsNavOpen(!isNavOpen);
   return (
     <>
       <Head>
         <title>{PageTitle}</title>
       </Head>
+      <main className="flex relative bg-gray-600">
+        <Sidebar />
+        <main
+          className={`bg-gray-200 min-h-screen rounded-tl-[3rem] border pt-9 px-10 ${
+            isNavOpen ? "openNav" : "closedNav"
+          }`}
+        >
+          <Topbar toggleNav={toggleNav} isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+          <section className="pt-10">{children}</section>
+        </main>
+      </main>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,5 @@
-import { Button, Drawer, Tooltip } from "@mui/material";
-import { blue } from "@mui/material/colors";
-import { useEffect, useState } from "react";
-import AppLogo from "../components/atoms/AppLogo";
-import Sidebar from "../components/molecules/Sidebar";
-import { BiChevronLeft, BiChevronRight, BiBell, BiLogIn } from "react-icons/bi";
-import Topbar from "../components/molecules/Topbar";
+import BaseLayout from "../components/templates/BaseLayout";
 
 export default function Home() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const toggleNav = () => setIsNavOpen(!isNavOpen);
-
-  return (
-    <main className="flex relative bg-gray-600">
-      <Sidebar />
-      <main
-        className={`bg-gray-200 min-h-screen rounded-tl-[3rem] border pt-9 px-10 ${
-          isNavOpen ? "openNav" : "closedNav"
-        }`}
-      >
-        <Topbar toggleNav={toggleNav} isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-      </main>
-    </main>
-  );
+  return <BaseLayout PageTitle="Dashboard">tes</BaseLayout>;
 }
